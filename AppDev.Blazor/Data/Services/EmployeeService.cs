f5b160d4e456ca5de96128fc9f73ed3a2f349150
Abhishek Humagain <abhishek.humagain@innovatetech.co>
2024-03-27 23:45:04 +0545

    used extension methog

--- AppDev.Blazor/Data/Services/EmployeeService.cs
+++ AppDev.Blazor/Data/Services/EmployeeService.cs
@@ -1,4 +1,5 @@
 using AppDev.Blazor.Data.DTO;
+using AppDev.Blazor.Data.Helper;
 using Newtonsoft.Json;
 
 namespace AppDev.Blazor.Data.Services;
@@ -6,7 +7,6 @@ namespace AppDev.Blazor.Data.Services;
 public class EmployeeService
 {
     private readonly HttpClient _httpClient;
-    private string baseUrl = "https://localhost:7190/";
     public EmployeeService(HttpClient httpClient)
     {
         _httpClient = httpClient;
@@ -14,8 +14,7 @@ public class EmployeeService
 
     public async Task<List<EmployeeData>?> GetEmployeeAsync()
     {
-        var response =  await _httpClient.GetAsync("https://localhost:7190/api/employee/all-employee");
-        
+        var response = await _httpClient.AuthGetAsync("https://localhost:7190/api/employee/all-employee");
         var result = response.Content.ReadAsStringAsync().Result;
         var rr = JsonConvert.DeserializeObject<List<EmployeeData>>(result);
         return rr;
